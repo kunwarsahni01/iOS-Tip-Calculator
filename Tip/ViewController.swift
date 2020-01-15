f813f6110787e341fd50587730b3cc3d9a0c74e4
Kunwar Sahni <kunwar.sahni01@gmail.com>
2020-01-15 18:08:37 -0500

    Initial Commit

---
+++ Tip/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tip
+//
+//  Created by Kunwar Sahni on 1/15/20.
+//  Copyright © 2020 purdue. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
